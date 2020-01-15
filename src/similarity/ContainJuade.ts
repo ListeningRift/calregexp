1f6d0b7b1b783781a99e1ad3a3be0a475af13333
Listenning Rift <83632199@qq.com>
2020-01-15 12:54:25 +0800

    Optimized the inclusion relation comparison method.

--- src/similarity/ContainJuade.ts
+++ src/similarity/ContainJuade.ts
@@ -2,7 +2,8 @@ import { Feature } from '../Objects'
 
 // 在进行特征比较提取之前，先进行包含关系判断，如果长字符串包括短字符串
 // 可以直接以短字符串为特征，减少多余计算
-function containJudge(strShort: string, strLong: string): boolean {
+function containJudge(str1: string, str2: string): boolean {
+    const [strLong, strShort] = str1.length > str2.length ? [str1, str2] : [str2, str1]
     if (strLong.length < strShort.length) {
         return false
     }
