b1e799c99c968e26ce9c52a56cf906f95ba4a874
Listenning Rift <83632199@qq.com>
2020-01-19 21:40:12 +0800

    Added DifferenceFeature class to general the differences

--- src/Objects.ts
+++ src/Objects.ts
@@ -1,22 +1,56 @@
-
 class SimilarityFeature {
-    private value: string
+    private feature: string
 
     constructor(feature: string) {
-        this.value = feature
+        this.feature = feature
     }
 
     public stringGet(): string {
-        return this.value
+        return this.feature
     }
 
     public regexpGet(): RegExp {
-        return new RegExp(this.value.split('').join('.*?'), 'g')
+        return new RegExp(this.feature.split('').join('.*?'), 'g')
     }
 
     public isEqual(otherFeature: SimilarityFeature): boolean {
-        return this.value === otherFeature.stringGet()
+        return this.feature === otherFeature.stringGet()
     }
 }
 
-export { SimilarityFeature }
+class DifferenceFeature {
+    private feature: string
+
+    constructor(feature: string) {
+        this.feature = feature
+    }
+
+    stringGet(): string {
+        return this.feature
+    }
+
+    regexpGet(): RegExp {
+        return new RegExp('[' + this.generalNumber() + this.generalLetter() + ']+')
+    }
+
+    generalNumber(): string {
+        if (this.feature.match(/[0-9]/)) {
+            return '0-9'
+        }
+        return ''
+    }
+
+    generalLetter(): string {
+        let res: string = ''
+        if (this.feature.match(/[a-z]/)) {
+            res += 'a-z'
+        }
+
+        if (this.feature.match(/[A-Z]/)) {
+            res += 'A-Z'
+        }
+        return res
+    }
+}
+
+export { SimilarityFeature, DifferenceFeature }
