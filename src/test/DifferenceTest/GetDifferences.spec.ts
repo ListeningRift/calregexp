e6484c6501fa4649154b3235c4ce8cb41e564560
Listenning Rift <83632199@qq.com>
2020-01-23 19:43:09 +0800

    Updated the function that gets the indexs, which can better align the feature.

--- src/test/DifferenceTest/GetDifferences.spec.ts
+++ src/test/DifferenceTest/GetDifferences.spec.ts
@@ -2,33 +2,60 @@ import { getAllCharsToIndex } from '../../difference'
 
 describe('test get all the chars to index', () => {
     test('test get all the chars to index 1', () => {
-        expect(getAllCharsToIndex('ada', 'abcdaa')).toStrictEqual(
+        expect(getAllCharsToIndex('ada', 'abcdaa')).toStrictEqual([
             {
-                'a': [0, 4, 5],
-                'd': [3]
+                'char': 'a',
+                'indexs': [0]
+            },
+            {
+                'char': 'd',
+                'indexs': [3]
+            },
+            {
+                'char': 'a',
+                'indexs': [4, 5]
             }
-        )
+        ])
     })
-
     test('test get all the chars to index 2', () => {
-        expect(getAllCharsToIndex('ushik', 'ushiuhskk')).toStrictEqual(
+        expect(getAllCharsToIndex('ushik', 'ushiuhskk')).toStrictEqual([
             {
-                'u': [0, 4],
-                's': [1, 6],
-                'h': [2, 5],
-                'i': [3],
-                'k': [7, 8]
+                'char': 'u',
+                'indexs': [0]
+            },
+            {
+                'char': 's',
+                'indexs': [1]
+            },
+            {
+                'char': 'h',
+                'indexs': [2]
+            },
+            {
+                'char': 'i',
+                'indexs': [3]
+            },
+            {
+                'char': 'k',
+                'indexs': [7, 8]
             }
-        )
+        ])
     })
 
     test('test get all the chars to index 3', () => {
-        expect(getAllCharsToIndex('dna', 'dfvbna')).toStrictEqual(
+        expect(getAllCharsToIndex('dna', 'dfvbna')).toStrictEqual([
             {
-                'd': [0],
-                'n': [4],
-                'a': [5]
-            }
-        )
+                'char': 'd',
+                'indexs': [0]
+            },
+            {
+                'char': 'n',
+                'indexs': [4]
+            },
+            {
+                'char': 'a',
+                'indexs': [5]
+            },
+        ])
     })
 })
